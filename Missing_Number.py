eaab5e6b481353754f728a13d052940451efed70
kim00451 <95480669+kim00451@users.noreply.github.com>
2022-10-29 00:05:06 -0400

    Weel6LeetCode

---
+++ Missing_Number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+        # at first, sort the array
+        nums.sort()
+
+        output = []
+        
+        for i in range(1, nums[-1]):
+            
+            if i not in nums:
+                
+                output.append(i)
+                
+        return output[i]
+
+        # https://thecleverprogrammer.com/2022/07/13/find-missing-number-using-python/
